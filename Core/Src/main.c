6ccfb46870e059999fb7403840eca99964e1eb09
Steven Guikal <void@fluix.one>
2022-10-24 15:05:18 -0400

    Correct GPIO mode and Pull-up/Pull-down of soft I2C pins

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -324,10 +324,17 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD2_Pin SOFT_SDA_Pin SOFT_SCL_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin|SOFT_SDA_Pin|SOFT_SCL_Pin;
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SOFT_SDA_Pin SOFT_SCL_Pin */
+  GPIO_InitStruct.Pin = SOFT_SDA_Pin|SOFT_SCL_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
